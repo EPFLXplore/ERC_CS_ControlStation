850f511c3f8478345dd066d60cda4328b9a7af64
Emile <emile.jd.hreich@gmail.com>
2020-11-09 17:17:03 +0100

    Science Layout V1.0

--- GUI/src/view.py
+++ GUI/src/view.py
@@ -24,68 +24,103 @@ class View:
 		style_context = Gtk.StyleContext()
 		style_context.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 		css = b"""
-       
-      
+
+		#SC{
+			color: #BEFF00;
+			font-weight: bold;
+			font-size: 15px;
+			border: 5px solid #BEFF00;
+
+		}
+		#WE{
+			color: #00FFD7;
+        	font-weight: bold;
+        	font-size: 15px;
+        	border: 5px solid #00FFD7;
+
+		}
+		#CI{
+			color:#CF00FF;
+        	font-weight: bold;
+        	font-size: 15px;
+        	border: 5px solid #CF00FF;
+
+		}
+		#SP{
+			color: #FF0037;
+        	font-weight: bold;
+        	font-size: 15px;
+        	border: 5px solid #FF0037;
+
+		}
+		#CA{
+			color: #FFB800;
+        	font-weight: bold;
+        	font-size: 15px;
+        	border: 5px solid #FFB800;
+
+		}
+
         #NAVBUTTON {
            border-radius: 35px;
            border: 5px solid #3464AB;
-           
+
         }
         #NAVBUTTON_CURRENT {
            border-radius: 35px;
            border: 5px solid Red;
-           
+
         }
         #SEPARATOR {
-           
+
            background-color: #1CEBD6;
-           
+
         }
-        
+
         #SWITCH {
            border-radius: 360px;
            border: 5px solid red;
            background: #FD8080;
-           
+
         }
-        
+
         #Idle{
-        
+
         	color: #4797CD;
         	font-weight: bold;
-        	font-size: 25px; 
+        	font-size: 25px;
         	border: 5px solid #4797CD;
-        	background: #141C31 
-        
+        	background: #141C31
+
         }
-        
+
         #Idle:hover {
           background: white;
-        
+
         }
         #Start:hover {
           background: white;
-        
+
         }
         #Working:hover {
           background: white;
-        
+
         }
         #Wait:hover {
           background: white;
-        
+
         }
         #Resume:hover {
           background: white;
-        
+
         }
         #Abort:hover {
           background: white;
-        
+
         }
         #Waiting:hover {
           background: white;
-        
+
         }
         #SWITCH:hover{
           background : #FFA9A9;
@@ -93,69 +128,69 @@ class View:
         #SWITCH:active{
           background : #FD8080;
         }
-        
+
         #Start{
-        
+
         	color: #0CEA13;
         	font-weight: bold;
-        	font-size: 25px; 
+        	font-size: 25px;
         	border: 5px solid #0CEA13;
-        	background: #141C31 
-        
+        	background: #141C31
+
         }
         #Working{
-        
+
         	color: #EBDD1A;
         	font-weight: bold;
-        	font-size: 25px; 
+        	font-size: 25px;
         	border: 5px solid #EBDD1A;
-        	background: #141C31 
-        
+        	background: #141C31
+
         }
         #Wait{
-        
+
         	color: #FCD306;
         	font-weight: bold;
-        	font-size: 25px; 
+        	font-size: 25px;
         	border: 5px solid #FCD306;
-        	background: #141C31 
-        
+        	background: #141C31
+
         }
         #Waiting{
-        
+
         	color: #D2119D;
         	font-weight: bold;
-        	font-size: 25px; 
+        	font-size: 25px;
         	border: 5px solid #D2119D;
-        	background: #141C31 
-                
+        	background: #141C31
+
         }
         #Resume{
-        
+
         	color: #2BAF17;
         	font-weight: bold;
-        	font-size: 25px; 
+        	font-size: 25px;
         	border: 5px solid #2BAF17;
-        	background: #141C31 
-        
+        	background: #141C31
+
         }
         #Abort{
-        
+
         	color: #FA0F2F;
         	font-weight: bold;
-        	font-size: 25px; 
+        	font-size: 25px;
         	border: 5px solid #FA0F2F;
-        	background: #141C31 
-        
+        	background: #141C31
+
         }
-        
-        
+
+
         #GRID {
-           
+
            border: 2px solid #3464AB;
-           
+
         }
-        
+
         #CAMERA_LABEL {
         	color: white;
         	font-weight: bold;
@@ -163,55 +198,69 @@ class View:
         	border-left: 2px solid #3464AB;
         	border-right: 2px solid #3464AB;
         }
-        
+
         #IMAGE {
-           
+
            border-bottom: 2px solid #3464AB;
-           
+
         }
-        
+
         #Tab-Label {
            background: #6C83B8;
            color: white;
            font-weight: bold;
-           font-size: 28px; 
-           border-top: 2px solid #3464AB; 
+           font-size: 28px;
+           border-top: 2px solid #3464AB;
         }
-        
+
         #AUTO {
-           
+
            color: Red;
            font-weight: bold;
-           font-size: 38px; 
-           border: 10px solid red; 
+           font-size: 38px;
+           border: 10px solid red;
         }
          #MANUAL {
-           
+
            color: Green;
            font-weight: bold;
-           font-size: 38px; 
-           border: 10px solid Green; 
+           font-size: 38px;
+           border: 10px solid Green;
         }
-        
+
         #Window_Top {
            background: #141C31;
            border-bottom: 2px solid #3464AB;
         }
-        
+
         #TAB_FRAME {
            background: #6C83B8;
         }
-        
+
         #DATA_FRAME {
         border: 2px solid #3464AB;
         }
+        #DATA_FRAME_2 {
+        border: 2px solid #00FFD7;
+        border-radius : 10px;
+        }
+		#DATA_FRAME_3{
+		border: 5px dashed #00FFD7;
+        border-radius : 360px;
+		}
+		#TUBE_BUTTON{
+			border-radius: 360px;
+			color: white;
+            font-weight: bold;
+            background: #3464AB;
+		}
         #DATA_FRAME_BG {
-        
+
         background-image: url("../resources/t.png");
         background-repeat: no-repeat;
         background-position: center;
         }
-        
+
         #frame1 {
            background: #141C31;
         }
@@ -244,19 +293,15 @@ class View:
 
 
 	def show_frame(self, *args):
+
 		self.ret, self.frame = self.capture.read()
 		self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-		
+
 		framecp = self.frame.copy()
-		
+
 		pb=GdkPixbuf.Pixbuf.new_from_data(framecp.tobytes(), GdkPixbuf.Colorspace.RGB, False, 8, framecp.shape[1], framecp.shape[0], framecp.shape[2]*framecp.shape[1])
-		
+
 		self.image1.set_from_pixbuf(pb.copy())
 		self.image2.set_from_pixbuf(pb.copy())
-		
+
 		return True
-
-
-
-
-
