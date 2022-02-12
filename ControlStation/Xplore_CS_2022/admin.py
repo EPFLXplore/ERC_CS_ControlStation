4692e99518e5e1dd9cef29591d4a36446b5e89e4
Roman <r.danylovych@gmail.com>
2022-02-12 19:00:10 +0100

    added the categories on website (just testing stuff)

--- ControlStation/Xplore_CS_2022/admin.py
+++ ControlStation/Xplore_CS_2022/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+admin.site.register(RoverConfirmation)
+admin.site.register(TaskProgress)
+admin.site.register(ScienceProgress)
+admin.site.register(Exception)
