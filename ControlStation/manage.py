8133cfde55c766833a2e855046e133e405bdfab0
ChristianMatala <christian.matala-tala@epfl.ch>
2022-03-23 13:07:36 +0100

    Working GUI

--- ControlStation/manage.py
+++ ControlStation/manage.py
@@ -3,12 +3,12 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ControlStation.settings')
     try:
         from django.core.management import execute_from_command_line
+        print("START LAUNCHER ==> http://127.0.0.1:8000/launcher")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
