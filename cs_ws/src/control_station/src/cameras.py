836d07e9b917b701e985e573aeb58d309b9f2107
Emile Janho Dit Hreich <emile.jd.hreich@gmail.com>
2022-09-03 02:49:36 +0200

    added cameras websockets

--- cs_ws/src/control_station/src/cameras.py
+++ cs_ws/src/control_station/src/cameras.py
@@ -26,10 +26,30 @@ from cv_bridge             import CvBridge
 
 # ==================================================================
 
-CAMERA_WS_URL = 'ws://127.0.0.1:8000/ws/video/wms/'
+CAMERA1_WS_URL = 'ws://127.0.0.1:8000/ws/video1/wms/'
+CAMERA2_WS_URL = 'ws://127.0.0.1:8000/ws/video2/wms/'
+CAMERA3_WS_URL = 'ws://127.0.0.1:8000/ws/video3/wms/'
+CAMERA4_WS_URL = 'ws://127.0.0.1:8000/ws/video4/wms/'
+CAMERA5_WS_URL = 'ws://127.0.0.1:8000/ws/video5/wms/'
+CAMERA6_WS_URL = 'ws://127.0.0.1:8000/ws/video6/wms/'
 
-ws = websocket.WebSocket()
-ws.connect(CAMERA_WS_URL)
+ws1 = websocket.WebSocket()
+ws1.connect(CAMERA1_WS_URL)
+
+ws2 = websocket.WebSocket()
+ws2.connect(CAMERA2_WS_URL)
+
+ws3 = websocket.WebSocket()
+ws3.connect(CAMERA3_WS_URL)
+
+ws4 = websocket.WebSocket()
+ws4.connect(CAMERA4_WS_URL)
+
+ws5 = websocket.WebSocket()
+ws5.connect(CAMERA5_WS_URL)
+
+ws6 = websocket.WebSocket()
+ws6.connect(CAMERA6_WS_URL)
 
 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),95]
 
@@ -43,19 +63,31 @@ im_6 = np.zeros((300, 300, 3))
 # ==================================================================
 # callback functions definition
 
-
-def display_cam_1(msg):
-    global im1 
-    im1 = bridge.compressed_imgmsg_to_cv2(msg)
-
-    result, imgencode = cv2.imencode('.jpg', im1, encode_param)
+def encode_stream(s):
+    result, imgencode = cv2.imencode('.jpg', s, encode_param)
     data = np.array(imgencode)
     img = data.tobytes()
     
     # base64 encoded transmission
     img = base64.b64encode(img).decode()
+
+    return img
+
+
+
+def display_cam_1(msg):
+    global im1 
+    im1 = bridge.compressed_imgmsg_to_cv2(msg)
+
+    # result, imgencode = cv2.imencode('.jpg', im1, encode_param)
+    # data = np.array(imgencode)
+    # img = data.tobytes()
     
-    ws.send("data:image/jpg;base64,"+ img)
+    # # base64 encoded transmission
+    # img = base64.b64encode(img).decode()
+    img = encode_stream(im1)
+    
+    ws1.send("data:image/jpg;base64,"+ img)
 
     
 
@@ -63,22 +95,47 @@ def display_cam_2(msg):
     global im2 
     im2 = bridge.compressed_imgmsg_to_cv2(msg)
 
+    img = encode_stream(im2)
+    
+    ws2.send("data:image/jpg;base64,"+ img)
+
+
 def display_cam_3(msg):
     global im3 
     im3 = bridge.compressed_imgmsg_to_cv2(msg)
+
+    img = encode_stream(im3)
+    
+    ws3.send("data:image/jpg;base64,"+ img)
+
     
 
 def display_cam_4(msg):
     global im4 
     im4 = bridge.compressed_imgmsg_to_cv2(msg)
 
+    img = encode_stream(im4)
+    
+    ws4.send("data:image/jpg;base64,"+ img)
+
+
 def display_cam_5(msg):
     global im5 
     im5 = bridge.compressed_imgmsg_to_cv2(msg)
 
+    img = encode_stream(im5)
+    
+    ws5.send("data:image/jpg;base64,"+ img)
+
+
 def display_cam_6(msg):
     global im6 
     im6 = bridge.compressed_imgmsg_to_cv2(msg)
+
+    img = encode_stream(im6)
+    
+    ws6.send("data:image/jpg;base64,"+ img)
+
 
 
 
