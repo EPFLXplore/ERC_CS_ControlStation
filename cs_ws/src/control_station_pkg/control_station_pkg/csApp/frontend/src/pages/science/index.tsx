5167c5eae4a7fa0b01e36b19830ab77a1c6e8b13
saraanej <sara.anejjar@epfl.ch>
2023-05-20 13:28:06 +0200

    Add bottom container

--- cs_ws/src/control_station_pkg/control_station_pkg/csApp/frontend/src/pages/science/index.tsx
+++ cs_ws/src/control_station_pkg/control_station_pkg/csApp/frontend/src/pages/science/index.tsx
@@ -72,6 +72,7 @@ export default () => {
 			<div className={styles.taskControlContainer}>
 				<TaskControl task={Task.SCIENCE} />
 			</div>
+			<div className={styles.InfoControllerContainer}></div>
 		</div>
 	);
 };
