2b66bb274e14888d1dbe4f8880d811895c9eebc8
epfl-xplore <epfl-xplore@gmail.com>
2023-07-18 22:33:12 +0200

    implement camera switching feature

--- cs_ws/src/rover_pkg/rover_pkg/cameras_publisher.py
+++ cs_ws/src/rover_pkg/rover_pkg/cameras_publisher.py
@@ -8,7 +8,7 @@ import cv2
 from cv_bridge import CvBridge
 
 
-CAMERA_FRAMERATE = 20
+CAMERA_FRAMERATE = 24
 
 
 class CamerasPublisher(Node):
@@ -27,52 +27,46 @@ class CamerasPublisher(Node):
         self.camera_0 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=0))
         self.camera_1 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=1))
         self.camera_2 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=2))
-        self.camera_3 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=3))
-        self.camera_4 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=4))
-        self.camera_5 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=5))
+        # self.camera_3 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=3))
+        # self.camera_4 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=4))
+        # self.camera_5 = cv2.VideoCapture(gstreamer_pipeline(sensor_id=5))
 
         self.bridge = CvBridge()
 
         self.camera_list = []
-        self.camera_list.append(self.camera_0)
-        self.camera_list.append(self.camera_1)
-        self.camera_list.append(self.camera_2)
-        self.camera_list.append(self.camera_3)
-        self.camera_list.append(self.camera_4)
-        self.camera_list.append(self.camera_5)
+        self.camera_list.append((self.camera_0, self.cam_1_pub))
+        self.camera_list.append((self.camera_1, self.cam_2_pub))
+        self.camera_list.append((self.camera_2, self.cam_3_pub))
+        # self.camera_list.append(self.camera_3)
+        # self.camera_list.append(self.camera_4)
+        # self.camera_list.append(self.camera_5)
 
 
         self.cam_publisher = []
         self.cam_publisher.append(self.cam_1_pub)
         self.cam_publisher.append(self.cam_2_pub)
         self.cam_publisher.append(self.cam_3_pub)
-        self.cam_publisher.append(self.cam_4_pub)
+        #self.cam_publisher.append(self.cam_4_pub)
 
-        self.update_camera = []
-
+        self.active_cameras = []
+        
         self.timer = self.create_timer(1/CAMERA_FRAMERATE, self.publish_feeds)
 
 
     def enable_camera(self, camera_index):
         print("Enabling camera: ", camera_index.data)
+        self.active_cameras = []
 
-        self.disable_camera()
         for i in camera_index.data:
-            self.update_camera.append(self.camera_list[i])
+            self.active_cameras.append(self.camera_list[i])
 
-    def disable_camera(self):
-        self.update_camera = []
-
-    def stop_camera(self):
-        for i in self.camera_list:
-            i.release()
 
     def publish_feeds(self):
 
-        for i in range(len(self.update_camera)):
-            ret, frame = self.update_camera[i].read()
+        for camera in self.active_cameras:
+            ret, frame = camera[0].read()
             if ret:
-                self.cam_publisher[i].publish(self.bridge.cv2_to_compressed_imgmsg(frame))
+                camera[1].publish(self.bridge.cv2_to_compressed_imgmsg(frame))
         
 
         # ret_0, frame_cam_0 = self.camera_0.read()
@@ -86,30 +80,30 @@ class CamerasPublisher(Node):
 
 def gstreamer_pipeline(
     sensor_id=0,
-    capture_width=1080,
-    capture_height=720,
-    display_width=1080,
-    display_height=720,
+    capture_width=500,
+    capture_height=500,
+    display_width=500,
+    display_height=500,
     framerate=CAMERA_FRAMERATE,
     flip_method=0,
 ):
-    # return (
-    #     "nvarguscamerasrc sensor-id=%d !"
-    #     "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-    #     "nvvidconv flip-method=%d ! "
-    #     "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-    #     "videoconvert ! "
-    #     "video/x-raw, format=(string)BGR ! appsink"
-    #     % (
-    #         sensor_id,
-    #         capture_width,
-    #         capture_height,
-    #         framerate,
-    #         flip_method,
-    #         display_width,
-    #         display_height,
-    #     )
-    # )
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
     # return (
     #         "gst-launch-1.0 v4l2src !"  
@@ -124,7 +118,7 @@ def gstreamer_pipeline(
     #         )
     # )
     #return ('sudo gst-launch-1.0 v4l2src ! videoconvert ! x264enc pass=qual quantizer=20 tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=8080')
-    return ('sudo gst-launch-1.0 v4l2src ! videoconvert ! video/x-raw, format=(string)BGR ! appsink')
+    #return ('sudo gst-launch-1.0 v4l2src ! videoconvert ! video/x-raw, format=(string)BGR ! appsink')
 
 def main(args=None):
 
