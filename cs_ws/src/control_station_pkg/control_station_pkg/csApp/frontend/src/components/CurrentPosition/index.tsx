b8e1a2e9542eb4dad5291ff4cc2f6bc533c74cf7
saraanej <sara.anejjar@epfl.ch>
2023-08-24 15:07:36 +0100

    current position decimals correction

--- cs_ws/src/control_station_pkg/control_station_pkg/csApp/frontend/src/components/CurrentPosition/index.tsx
+++ cs_ws/src/control_station_pkg/control_station_pkg/csApp/frontend/src/components/CurrentPosition/index.tsx
@@ -8,9 +8,9 @@ export default function MyComponent({ currentPoint }: Props) {
 	return (
 		<div className={styles.container}>
 			<div className={styles.containerValues}>
-				<div className={styles.coordinates}>{currentPoint[0]}, </div>
-				<div className={styles.coordinates}>{currentPoint[1]}, </div>
-				<div className={styles.coordinates}>{currentPoint[2]}°</div>
+				<div className={styles.coordinates}>{currentPoint[0].toFixed(2)}, </div>
+				<div className={styles.coordinates}>{currentPoint[1].toFixed(2)}, </div>
+				<div className={styles.coordinates}>{currentPoint[2].toFixed(2)}°</div>
 			</div>
 		</div>
 	);
