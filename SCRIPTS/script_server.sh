3ed7f32113b76ba30adfd51f03bb410983c1dfba
epfl-xplore <100864969+epfl-xplore@users.noreply.github.com>
2023-12-13 17:38:42 +0100

    Update script_server.sh

--- SCRIPTS/script_server.sh
+++ SCRIPTS/script_server.sh
@@ -1,7 +1,7 @@
 killall redis-server
 sudo service redis-server stop
 redis-server &
-cd /home/evan/Desktop/Xplore/CS_workspace/cs_ws
+cd ../cs_ws
 colcon build
 source install/setup.bash
 cd src/control_station_pkg/control_station_pkg
