6565440260d23563c2c7a72ca015fc367217acc8
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2024-04-26 14:37:49 +0200

    fixes

---
+++ docker_humble_desktop/run_cs_mac.sh
@@ -0,0 +1,34 @@
+# If not working, first do: sudo rm -rf /tmp/.docker.xauth
+# If still not working, try running the script as root.
+
+XAUTH=/tmp/.docker.xauth
+
+echo "Preparing Xauthority data..."
+xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
+if [ ! -f $XAUTH ]; then
+    if [ ! -z "$xauth_list" ]; then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+echo "Done."
+echo ""
+echo "Verifying file contents:"
+file $XAUTH
+echo "--> It should say \"X11 Xauthority data\"."
+echo ""
+echo "Permissions:"
+ls -FAlh $XAUTH
+echo ""
+echo "Running docker..."
+
+# Get the current working directory
+current_dir=$(pwd)
+
+export XAUTH=$XAUTH
+export PARENT_DIR=$(dirname "$current_dir")
+
+docker compose up
