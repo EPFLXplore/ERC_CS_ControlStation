886f159c6e1e278ee7982c6fd52875be7b735629
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2024-06-09 18:38:22 +0200

    start frontent with service

--- launch.sh
+++ launch.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
 cd ..
 colcon build
 source install/setup.bash
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml & (cd frontend && npm start)
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml & (cd src/frontend && npm start)
