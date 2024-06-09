c995c900473d049aa102ce765829693ebfbd11d0
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2024-06-09 15:34:04 +0200

    Clean Repo

--- cs_ws/src/control_station_pkg/control_station_pkg/csApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(RoverConfirmation)
-admin.site.register(TaskProgress)
-#admin.site.register(Science)
-admin.site.register(Exception)
-admin.site.register(Navigation)
--- cs_ws/src/control_station_pkg/control_station_pkg/csApp/routing.py
+++
@@ -1,8 +0,0 @@
-# csApp/routing.py
-from django.urls import path
-from .Consumers import RoverConsumer, GamepadConsumer
-
-websocket_urlpatterns = [
-    path('ws/csApp/rover_state/', RoverConsumer.RoverConsumer.as_asgi()),
-    path('ws/csApp/gamepad/', GamepadConsumer.GamepadConsumer.as_asgi())
-]
