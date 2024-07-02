20848899777773c81c82cb23b86f9b237ece936f
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2024-07-02 18:56:37 +0200

    Update frontend and make rosbridge compatible with actions ros2

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 cd ..
 colcon build
 source install/setup.bash
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml & (cd src/frontend && npm start)
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml call_services_in_new_thread:=true send_action_goals_in_new_thread:=true & (cd src/frontend && npm start)
