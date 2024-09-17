546ea894bfebafb99467ae257e8084aff11c11f5
flxinxout <giovanni.ranieri@epfl.ch>
2024-09-17 21:05:55 +0200

    start cs file in 1 click bordel de merde

--- start_cs.sh
+++ start_cs.sh
@@ -1,2 +1,7 @@
-cd /home/xplore/ERC_CS_ControlStation/docker_humble_desktop
+cd docker_humble_desktop
 ./run_cs.sh
+
+sleep(10)
+
+docker exec cs_humble_desktop cd src
+docker exer cs_humble_desktop ./launch.sh
