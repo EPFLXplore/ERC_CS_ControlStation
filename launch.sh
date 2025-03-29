9b43e96301cd0c8525e5ad98597b334c58c52e77
flxinxout <giovanni.ranieri@epfl.ch>
2025-03-29 20:08:13 +0100

    launch file

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@ cd ..
 source /opt/ros/humble/setup.bash
 colcon build
 source install/setup.bash
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml call_services_in_new_thread:=true send_action_goals_in_new_thread:=true & (cd src/frontend && npm start) & (cd src/frontend/ssh_backend && node ssh_server.js)
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml call_services_in_new_thread:=true send_action_goals_in_new_thread:=true & (cd src/frontend && npm start)
