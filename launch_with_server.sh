c52991758c0943bf28caa7e668eea6b2dcd153d2
flxinxout <giovanni.ranieri@epfl.ch>
2025-06-04 22:11:13 +0200

    freontend and add conpression

--- launch_with_server.sh
+++ launch_with_server.sh
@@ -3,4 +3,4 @@ source /opt/ros/humble/setup.bash
 colcon build
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 source install/setup.bash
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml call_services_in_new_thread:=true send_action_goals_in_new_thread:=true & (cd src/frontend && npm start) & (cd src/frontend/ssh_backend && node ssh_server.js)
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml use_compression:=true call_services_in_new_thread:=true send_action_goals_in_new_thread:=true & (cd src/frontend && npm start) & (cd src/frontend/ssh_backend && node ssh_server.js)
