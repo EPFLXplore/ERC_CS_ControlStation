db64ee3ede79c6a420f2634a79f7fda39e3e9383
Mehdi Alaoui <mehdi.ala03@gmail.com>
2026-04-25 14:49:08 +0200

    fixes microscope and UI changes

--- docker_humble_desktop/run.sh
+++ docker_humble_desktop/run.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
 # If not working, first do: sudo rm -rf /tmp/.docker.xauth
 # If still not working, try running the script as root.
 echo "Launching Chrome at http://localhost:3000/ ..."
 google-chrome --new-window "http://localhost:3000/" >/dev/null 2>&1 &
 sleep 1
-
 XAUTH=/tmp/.docker.xauth
-
 echo "Preparing Xauthority data..."
 xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
 if [ ! -f $XAUTH ]; then
@@ -16,7 +15,6 @@ if [ ! -f $XAUTH ]; then
     fi
     chmod a+r $XAUTH
 fi
-
 echo "Done."
 echo ""
 echo "Verifying file contents:"
@@ -27,24 +25,17 @@ echo "Permissions:"
 ls -FAlh $XAUTH
 echo ""
 echo "Running docker..."
-
 if docker ps --format '{{.Names}}' | grep -Fxq cs_humble_desktop; then
     echo "Container cs_humble_desktop is already running. Opening a shell in it..."
-    docker exec -it cs_humble_desktop bash
+    docker exec -it -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp -e CYCLONEDDS_URI="file:///cyclone.xml" cs_humble_desktop bash
     exit 0
 fi
-
 if docker ps -a --format '{{.Names}}' | grep -Fxq cs_humble_desktop; then
     echo "Removing stale container cs_humble_desktop..."
     docker rm cs_humble_desktop >/dev/null
 fi
-
-# Get the current working directory
 current_dir=$(pwd)
-
-# Use dirname to get the parent directory
 parent_dir=$(dirname "$current_dir")
-
 docker run -it \
     --name cs_humble_desktop \
     --rm \
@@ -59,12 +50,11 @@ docker run -it \
     -v /dev:/dev \
     -v $parent_dir:/home/xplore/dev_ws/src \
     -v cs_humble_desktop_home_volume:/home/xplore \
+    -v /home/mehdi/ERC_CS_ControlStation/docker_humble_desktop/cyclonedds.xml:/cyclone.xml:ro \
+    -e CYCLONEDDS_URI="file:///cyclone.xml" \
+    -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
     ghcr.io/epflxplore/cs:humble-desktop \
-    bash -lc "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp; \
-              cd /home/xplore/dev_ws/src; \
+    bash -lc "cd /home/xplore/dev_ws/src; \
               chmod +x ./launch_with_server.sh; \
               ./launch_with_server.sh; \
               exec bash"
-
-    # -v ~/Documents/xplore/ERC_CS_ControlStation/docker_humble_desktop/cyclonedds.xml:/home/xplore/cyclonedds.xml:ro \
-    # -e CYCLONEDDS_URI="file:///home/xplore/cyclonedds.xml" \
